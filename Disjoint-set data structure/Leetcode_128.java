2a392752144dcb7063850891b60e4ac877cc2b9e
yeeh1616 <50901300+yeeh1616@users.noreply.github.com>
2021-10-07 09:46:40 +0200

    Update and rename Leetcode_128 to Leetcode_128.java

--- Disjoint-set data structure/Leetcode_128.java
+++ Disjoint-set data structure/Leetcode_128.java
@@ -1,4 +1,4 @@
-128. Longest Consecutive Sequence
+/*128. Longest Consecutive Sequence
 Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
 You must write an algorithm that runs in O(n) time.
 
@@ -10,8 +10,8 @@ Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefor
 Example 2:
 
 Input: nums = [0,3,7,2,5,8,4,6,0,1]
-Output: 9
----------------------------------------------------- Solution ----------------------------------------------------
+Output: 9*/
+/*---------------------------------------------------- Solution ----------------------------------------------------*/
 
 class Solution {
     public int longestConsecutive(int[] nums) {
