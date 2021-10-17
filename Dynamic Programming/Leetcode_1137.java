d1b7a92602a3966d91a9eb44998ade58443ea6c6
yeeh1616 <50901300+yeeh1616@users.noreply.github.com>
2021-10-17 11:39:17 +0200

    Create Leetcode_1137.java

---
+++ Dynamic Programming/Leetcode_1137.java
@@ -0,0 +1,79 @@
+// N-th Tribonacci Number
+/*
+The Tribonacci sequence Tn is defined as follows: 
+
+T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+
+Given n, return the value of Tn.
+
+ 
+
+Example 1:
+
+Input: n = 4
+Output: 4
+Explanation:
+T_3 = 0 + 1 + 1 = 2
+T_4 = 1 + 1 + 2 = 4
+Example 2:
+
+Input: n = 25
+Output: 1389537
+*/
+
+// 1. Dynamic Programming
+class Solution {
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n <= 2) {
+            return 1;
+        }
+        int p = 0, q = 0, r = 1, s = 1;
+        for (int i = 3; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = s;
+            s = p + q + r;
+        }
+        return s;
+    }
+}
+
+// 2. 矩阵快速幂
+class Solution {
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n <= 2) {
+            return 1;
+        }
+        int[][] q = {{1, 1, 1}, {1, 0, 0}, {0, 1, 0}};
+        int[][] res = pow(q, n);
+        return res[0][2];
+    }
+
+    public int[][] pow(int[][] a, int n) {
+        int[][] ret = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                ret = multiply(ret, a);
+            }
+            n >>= 1;
+            a = multiply(a, a);
+        }
+        return ret;
+    }
+
+    public int[][] multiply(int[][] a, int[][] b) {
+        int[][] c = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j] + a[i][2] * b[2][j];
+            }
+        }
+        return c;
+    }
+}
