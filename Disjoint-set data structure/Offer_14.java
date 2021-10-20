a7d03f3e1703b09bf31491b616848b4aa7f90944
yeeh1616 <50901300+yeeh1616@users.noreply.github.com>
2021-10-20 22:24:54 +0200

    Create Offer_14.java

---
+++ Disjoint-set data structure/Offer_14.java
@@ -0,0 +1,9 @@
+/*
+Cut a rope into m pieces, of which the lenght is n.
+
+Key point:
+1. Using dynamic programming,
+2. There must be a table for store states of subproblem (a state is the optimal solution of each subproblem)
+3. The state table must has a couple of init values, and the other solutions are calculated based on these values.
+*/
+
