8713e7c852df48f6fb32f4f47117ca6366478750
yeeh1616 <50901300+yeeh1616@users.noreply.github.com>
2021-10-23 11:01:34 +0200

    Create Leetcode_119.java

---
+++ Dynamic Programming/Leetcode_119.java
@@ -0,0 +1,36 @@
+/*
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+Example 1:
+
+Input: rowIndex = 3
+Output: [1,3,3,1]
+Example 2:
+
+Input: rowIndex = 0
+Output: [1]
+Example 3:
+
+Input: rowIndex = 1
+Output: [1,1]
+*/
+
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> C = new ArrayList<List<Integer>>();
+        for (int i = 0; i <= rowIndex; ++i) {
+            List<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(C.get(i - 1).get(j - 1) + C.get(i - 1).get(j));
+                }
+            }
+            C.add(row);
+        }
+        return C.get(rowIndex);
+    }
+}
