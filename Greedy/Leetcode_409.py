0c6c79862d5f05b4bd7a16488dad3cfa260f879a
yeeh1616 <50901300+yeeh1616@users.noreply.github.com>
2022-01-15 20:04:59 +0100

    Create Leetcode_409.py

---
+++ Greedy/Leetcode_409.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        ans = 0
+        count = collections.Counter(s)
+        for v in count.values():
+            ans += v // 2 * 2
+            if ans % 2 == 0 and v % 2 == 1:
+                ans += 1
+        return ans
+
+      
