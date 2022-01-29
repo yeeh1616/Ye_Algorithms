9a31c87ecfc8245189c78f7f12bf0e4033eaca37
yeeh1616 <50901300+yeeh1616@users.noreply.github.com>
2022-01-30 00:21:15 +0100

    Create Leetcode_581.java

---
+++ Greedy/Leetcode_581.java
@@ -0,0 +1,43 @@
+/* 精简答案：https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray/solution/si-lu-qing-xi-ming-liao-kan-bu-dong-bu-cun-zai-de-/
+581. Shortest Unsorted Continuous Subarray
+Given an integer array nums, you need to find one continuous subarray that if you only sort this subarray in ascending order, then the whole array will be sorted in ascending order.
+Return the shortest such subarray and output its length.
+
+Example 1:
+Input: nums = [2,6,4,8,10,9,15]
+Output: 5
+Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: 0
+
+Example 3:
+Input: nums = [1]
+Output: 0
+*/
+
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        //初始化
+        int len = nums.length;
+        int min = nums[len-1];
+        int max = nums[0];
+        int begin = 0, end = -1;
+        //遍历
+        for(int i = 0; i < len; i++){
+            if(nums[i] < max){      //从左到右维持最大值，寻找右边界end
+                end = i;
+            }else{
+                max = nums[i];
+            }
+            
+            if(nums[len-i-1] > min){    //从右到左维持最小值，寻找左边界begin
+                begin = len-i-1;
+            }else{
+                min = nums[len-i-1];
+            }            
+        }
+        return end-begin+1;
+    }
+}
