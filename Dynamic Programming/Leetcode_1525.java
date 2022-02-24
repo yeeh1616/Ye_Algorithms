8d0e087b5b2c868d7000e50bf3d3aa8e887fca2b
yeeh1616 <50901300+yeeh1616@users.noreply.github.com>
2022-02-24 13:06:58 +0100

    Create Leetcode_1525.java

---
+++ Dynamic Programming/Leetcode_1525.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int numSplits(String s) {
+        int n = s.length();
+        int[] left = new int[n + 2];
+        int[] right = new int[n + 2];
+        boolean[] recLeft = new boolean[26];
+        boolean[] recRight = new boolean[26];
+        for (int i = 1; i <= n; i++) {
+            int c = s.charAt(i - 1) - 'a';
+            if (recLeft[c]) {
+                left[i] = left[i - 1];
+            } else {
+                recLeft[c] = true;;
+                left[i] = left[i - 1] + 1;
+            }
+        }
+        for (int i = n; i > 0; i--) {
+            int c = s.charAt(i - 1) - 'a';
+            if (recRight[c]) {
+                right[i] = right[i + 1];
+            } else {
+                recRight[c] = true;
+                right[i] = right[i + 1] + 1;
+            }
+        }
+        int ret = 0;
+        for (int i = 1; i < n; i++) {
+            if (left[i] == right[i + 1]) {
+                ret++;
+            }
+        }
+        return ret;
+    }
+}
