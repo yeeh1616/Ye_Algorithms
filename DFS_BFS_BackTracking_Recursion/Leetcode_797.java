d0ee6a15bca584c407d5d14e2b452d6b194b7928
yeeh1616 <50901300+yeeh1616@users.noreply.github.com>
2022-03-01 16:32:24 +0100

    Update Leetcode_797.java

--- DFS_BFS_BackTracking_Recursion/Leetcode_797.java
+++ DFS_BFS_BackTracking_Recursion/Leetcode_797.java
@@ -30,3 +30,27 @@ class Solution {
 
 
 // BFS
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        que = collections.deque()
+        que.append([0])
+        paths = []
+        #每一次都是作为整个路径来进行存储的
+        while que:
+            tmp_path = que.popleft()
+        #跳出最后一个元素 如果是最后的元素 就直接添加路径
+            node = tmp_path[-1]
+            if node == len(graph) - 1:
+                paths.append(tmp_path)
+        #遍历对应节点的graph
+            for i in graph[node]:
+                tmp_path.append(i)
+                que.append(tmp_path.copy())
+                tmp_path.pop()
+        return paths
+
+
+作者：dudu-21
+链接：https://leetcode-cn.com/problems/all-paths-from-source-to-target/solution/bfs-12yue-bu-bai-lan-by-dudu-21-7xcb/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
