e0383a7353f42d3b7c8e8730aa6dd5dda1bbba66
yeeh1616 <50901300+yeeh1616@users.noreply.github.com>
2022-03-03 00:19:57 +0100

    Update Leetcode_547.java

--- DFS_BFS_BackTracking_Recursion/Leetcode_547.java
+++ DFS_BFS_BackTracking_Recursion/Leetcode_547.java
@@ -34,3 +34,95 @@ class Solution {
 链接：https://leetcode-cn.com/problems/number-of-provinces/solution/dfs-bfs-bing-cha-ji-3-chong-fang-fa-ji-s-edkl/
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+// BFS
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        // int[][] isConnected 是无向图的邻接矩阵，n 为无向图的顶点数量
+        int n = isConnected.length;
+        // 定义 boolean 数组标识顶点是否被访问
+        boolean[] visited = new boolean[n];
+        
+        // 定义 cnt 来累计遍历过的连通域的数量
+        int cnt = 0;  
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            // 若当前顶点 i 未被访问，说明又是一个新的连通域，则bfs新的连通域且cnt+=1.
+            if (!visited[i]) {
+                cnt++;
+                queue.offer(i);
+                visited[i] = true;
+                while (!queue.isEmpty()) {
+                    int v = queue.poll();
+                    for (int w = 0; w < n; w++) {
+                        if (isConnected[v][w] == 1 && !visited[w]) {
+                            visited[w] = true;
+                            queue.offer(w);
+                        }
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+} 
+
+作者：sweetiee
+链接：https://leetcode-cn.com/problems/number-of-provinces/solution/dfs-bfs-bing-cha-ji-3-chong-fang-fa-ji-s-edkl/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+// 并查集
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        // 初始化并查集
+        UnionFind uf = new UnionFind(n);
+        // 遍历每个顶点，将当前顶点与其邻接点进行合并
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isConnected[i][j] == 1) {
+                    uf.union(i, j);
+                }
+            }
+        }
+        // 返回最终合并后的集合的数量
+        return uf.size;
+    }
+}
+
+// 并查集
+class UnionFind {
+    int[] roots;
+    int size; // 集合数量
+    
+    public UnionFind(int n) {
+        roots = new int[n];
+        for (int i = 0; i < n; i++) {
+            roots[i] = i;
+        }
+        size = n;
+    }
+
+    public int find(int i) {
+       if (i == roots[i]) {
+           return i;
+       }
+       return roots[i] = find(roots[i]);
+    }
+
+    public void union(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if (pRoot != qRoot) {
+            roots[pRoot] = qRoot;
+            size--;
+        }
+    }
+}
+
+作者：sweetiee
+链接：https://leetcode-cn.com/problems/number-of-provinces/solution/dfs-bfs-bing-cha-ji-3-chong-fang-fa-ji-s-edkl/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
