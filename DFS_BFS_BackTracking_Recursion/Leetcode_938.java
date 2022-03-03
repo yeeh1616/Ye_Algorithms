5597f370fd918b152ce4b62c0679d04a32f8c03c
yeeh1616 <50901300+yeeh1616@users.noreply.github.com>
2022-03-03 22:08:18 +0100

    Create Leetcode_938.java

---
+++ DFS_BFS_BackTracking_Recursion/Leetcode_938.java
@@ -0,0 +1,50 @@
+// DFS
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        }
+        if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        }
+        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+    }
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/range-sum-of-bst/solution/er-cha-sou-suo-shu-de-fan-wei-he-by-leet-rpq7/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+// BFS
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        int sum = 0;
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if (node == null) {
+                continue;
+            }
+            if (node.val > high) {
+                q.offer(node.left);
+            } else if (node.val < low) {
+                q.offer(node.right);
+            } else {
+                sum += node.val;
+                q.offer(node.left);
+                q.offer(node.right);
+            }
+        }
+        return sum;
+    }
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/range-sum-of-bst/solution/er-cha-sou-suo-shu-de-fan-wei-he-by-leet-rpq7/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
