92f9c9b84a08a2e5d28f94b038e777bc7773bb22
yeeh1616 <50901300+yeeh1616@users.noreply.github.com>
2022-03-10 23:23:32 +0100

    Update Leetcode_409.py

--- Greedy/Leetcode_409.py
+++ Greedy/Leetcode_409.py
@@ -9,3 +9,22 @@ class Solution:
         return ans
 
       
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char,int> mp;
+        for(auto& c:s){
+            mp[c]++;
+        }
+        int ans=0,f=0;
+        for(auto& [_,cnt]:mp){
+            ans+=cnt%2==0?cnt:(f=1,cnt-1);
+        }
+        return ans+f;
+    }
+};
+
+作者：qiu-yu-mei-xue
+链接：https://leetcode-cn.com/problems/longest-palindrome/solution/c-zui-jian-tan-xin-by-qiu-yu-mei-xue-zj2k/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
