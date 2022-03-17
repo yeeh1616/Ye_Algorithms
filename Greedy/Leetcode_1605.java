fef3d7c3a7e58793da9f32671874d930f84dd40e
yeeh1616 <50901300+yeeh1616@users.noreply.github.com>
2022-03-17 23:42:35 +0100

    Create Leetcode_1605.java

---
+++ Greedy/Leetcode_1605.java
@@ -0,0 +1,8 @@
+// 万万没想到！
+// v = min(row[i],col[j])
+// g[i][j]=v
+// row[i] -= v, col[j] -= v
+// 如果row[i]>0,搞g[i][j+1]
+// 如果col[j]>0,搞g[i+1][j]
+// 都为0搞g[i+1][j+1]
+https://leetcode-cn.com/problems/find-valid-matrix-given-row-and-column-sums/solution/tan-xin-fa-fu-tu-jie-bao-zheng-neng-dong-by-durant/
