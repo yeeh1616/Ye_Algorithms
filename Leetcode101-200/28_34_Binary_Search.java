18f5163648e96eb04b582dd8cd6fd89c24567c83
yeeh1616 <50901300+yeeh1616@users.noreply.github.com>
2022-04-04 21:46:11 +0200

    Update 28_34_Binary_Search.java

--- Leetcode101-200/28_34_Binary_Search.java
+++ Leetcode101-200/28_34_Binary_Search.java
@@ -1,1 +1,51 @@
 // 卒
+// 改了尼玛的一万遍
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+
+        if(n==0){
+            return new int[]{-1,-1};
+        }
+
+        if(n==1){
+            if(nums[0]==target){
+                return new int[]{0,0};
+            }else {
+                return new int[]{-1,-1};
+            }
+        }
+
+        int left = 0;
+        int right = n-1;
+        int mid = 0;
+
+        while (left<right){
+            mid = (left+right)/2;
+
+            if(nums[mid] < target){
+                left = mid+1;
+            }else if(nums[mid] > target){
+                right = mid;
+            }else {
+                break;
+            }
+        }
+        mid = (left+right)/2;//天地之精华
+
+        if(nums[mid]!=target){
+            return new int[]{-1, -1};
+        }
+
+        left = mid;
+        right = mid;
+        while (left-1 >= 0 && nums[left-1]==target){
+            left--;
+        }
+        while (right+1 < n && nums[right+1]==target){
+            right++;
+        }
+
+        return new int[]{left, right};
+    }
+}
