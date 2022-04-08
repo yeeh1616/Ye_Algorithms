e3fe265724f07d6e7793f67df68767637ea5a33d
yeeh1616 <50901300+yeeh1616@users.noreply.github.com>
2022-04-08 23:11:52 +0200

    Create 33_720_Hashtable.java

---
+++ Leetcode101-200/33_720_Hashtable.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String longestWord(String[] words) {
+        Arrays.sort(words, (a, b) ->  {
+            if (a.length() != b.length()) {
+                return a.length() - b.length();
+            } else {
+                return b.compareTo(a);
+            }
+        });
+        String longest = "";
+        Set<String> candidates = new HashSet<String>();
+        candidates.add("");
+        int n = words.length;
+        for (int i = 0; i < n; i++) {
+            String word = words[i];
+            if (candidates.contains(word.substring(0, word.length() - 1))) {
+                candidates.add(word);
+                longest = word;
+            }
+        }
+        return longest;
+    }
+}
